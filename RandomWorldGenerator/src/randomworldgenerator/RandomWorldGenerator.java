4ff8546f1f69d042f751a8c070433be751dafae5
nasrallah <nasaroanas6@gmail.com>
2024-05-15 11:09:57 +0200

    Update RandomWorldGenerator.java

--- RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
+++ RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
@@ -16,7 +16,7 @@ public class RandomWorldGenerator {
      */
     public static void main(String[] args) {
         System.out.println("Hola jose");
-        System.out.println("Que locura el git");
+        System.out.println("Que locura el git ");
     }
     
 }
